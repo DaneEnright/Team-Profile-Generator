e635c3132552cb96421ffd9f0a049bbf66ff5cb2
Dane Enright <enrightco@comcast.net>
2021-05-14 15:17:15 -0600

    started file

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateHtml = require("./generateHtml")
+
+const questions = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+
+
+
+
+]
+
