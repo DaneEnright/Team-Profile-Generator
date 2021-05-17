a8f217004aec5b43878079cafac51af8dd97a500
Dane Enright <enrightco@comcast.net>
2021-05-16 20:25:59 -0600

    added function create Manager and library directory

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-const generateHtml = require("./generateHtml")
+const generateHtml = require("./generateHtml");
+const Manager = require("./library/manager");
 
-const questions = [
-    {
+let teamMembers = [];
 
-    },
-    {
+function app() {
+  function createManager(params) {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your team manager's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your employee id?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email?",
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is your Office Phone Number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        teamMembers.push(manager);
+      });
+  }
 
-    },
-    {
+  createManager();
+}
+app();
 
-    },
-    {
+function createTeam() {}
 
-    },
+function addEngineer() {}
 
+function addIntern() {}
 
-
-
-]
-
+function generateHtml() {}
